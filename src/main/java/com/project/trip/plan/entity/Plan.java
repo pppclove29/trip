4d7772915458c571c9d52d21c5accbd30df8ea4a
pppclove-notebook <pppclove29@naver.com>
2023-05-06 18:17:22 +0900

    asd

--- src/main/java/com/project/trip/plan/entity/Plan.java
+++ src/main/java/com/project/trip/plan/entity/Plan.java
@@ -1,22 +1,22 @@
-//package com.project.trip.plan.entity;
-//
-//
-//import com.project.trip.user.entity.User;
-//import jakarta.persistence.*;
-//
-//@Entity
-//public class Plan {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @Column(name = "PLAN_ID", nullable = false)
-//    private int id;
-//
-//    private String title;
-//    private String content;
-//    private String image;
-//
-//    @JoinColumn(name = "USER_ID")
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private User writer;
-//}
+package com.project.trip.plan.entity;
+
+
+import com.project.trip.user.entity.User;
+import jakarta.persistence.*;
+
+@Entity
+public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "PLAN_ID", nullable = false)
+    private int id;
+
+    private String title;
+    private String content;
+    private String image;
+
+    @JoinColumn(name = "USER_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User writer;
+}
