8f27dbc55ae182afcf09f11d036b4f0f2d8bfe8d
pppclove29 <pppclove29@naver.com>
2023-05-12 14:39:01 +0900

    failuer 설정

--- src/main/java/com/project/trip/global/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/project/trip/global/config/CustomAuthenticationFailureHandler.java
@@ -7,11 +7,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         System.out.println("Login Failed: " + exception.getMessage());
+        System.out.println(exception.getCause().toString());
+        System.out.println(Arrays.toString(exception.getStackTrace()));
 
         System.out.println("뭔가 로그인 실패!!!!");
 
